9dbab8e51104a1cab1f62c3f2c78ef2337dd7a28
David Ehnebuske <dehne1@comcast.net>
2022-03-05 15:16:28 -0800

    Get controller tty interfact working

--- MediaPlayer.c
+++ MediaPlayer.c
@@ -1,27 +1,80 @@
 /***
+ * MediaPlayer Version 0.10, February 2022
  * 
- * MediaPlayer Version 0.10
+ * The media clip player for the PTMSC Pinto Abalone exhibit.
  * 
- * The mediaplayer for the PTMSC Pinto Abalone exhibit.
+ * The media player plays video clips in response to commands from the main 
+ * controller for the exhibit. Since it's attached to the exhibit's monitor 
+ * and runs on a Pi, it also serves as a keyboard, mouse and monitor 
+ * interface to the system. It can interpret a set of commands typed to it 
+ * and will forward commands to the controller and display the controller's 
+ * response.
  * 
- ***/
+ * The clips are all contained in one big video file. The location of the file 
+ * and the description of the clips are contained in the file mediadf.h See 
+ * there for details, but basically a clip is defined by its start and end 
+ * time and information on how it is to be played. All of the complicated 
+ * stuff about actually playing videos is handled by the open source VLC media 
+ * player. From that point of view, MediaPlayer is nothing more than a custom 
+ * interface adapted to our purpose that's built on top of VLC.
+ * 
+ * Media player's operation -- e.g., which clip it plays -- is done by sending 
+ * it commands. Commands can come from the keyboard attached to stdin  (if any)
+ * or they can come from the controller attached to CONTROLLER_TTY. Commands 
+ * from the controller begin with "!" to distinguish them from diagnostic and 
+ * informational messages from the controller. All messages from the 
+ * controller, prefixed by "[controller] ", are echoed on stdout.
+ * 
+ * A line of inout from the keyboard is interpreted to be a command directed 
+ * at MediaPlayer unless it starts with "!" in which case it is stripped of 
+ * its leading "!" and sent to the controller. The idea is to allow the person 
+ * at the keyboard to directly issue commands to the controller.
+ * 
+ ***
+ * 
+ * Copyright (C) 2020-2022 D.L. Ehnebuske
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. 
+ * 
+***/
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <termios.h>
 #include <wiringPi.h>
 #include <vlc/vlc.h>
-#include <unistd.h>
 
 #include "mediadef.h"                               // Definition of the media clip
 
 #define CONTROLLER_TTY  "/dev/ttyACM0"              // The tty we use to talk to the exhibit controller
-#define MAX_LINE_LENGTH (512)                       // The maximum length of a user's input (chars)
+#define MAX_LINE_LENGTH (128)                       // The maximum length of a user's input (chars)
 #define MAX_WORDS       (3)                         // The maximum number of words in a command line (chars)
 #define MAX_WSIZE       (20)                        // The maximum length of a word (chars)
-#define SLEEP_US        (100000)                    // Number of uSec to sleep whe some time needt to pass
+#define SLEEP_US        (100)                       // Number of uSec to sleep whe some time needs to pass (1/30 sec)
+#define MEDIA_DELTA_MS  (600)                       // The Adobe Rush times - VLC times in ms (18 frames)
 #define BANNER          "PTMSC Pinto Abalone Exhibit Media Player v0.1, February 2022"
 #define CMD_SET_VERS    (1000)                      // The version of the command set we speak with the controller
+
+// piLock() / piUnlock() usage
+#define LOCK_CLIP       (0)                         // piLock(0) is for changing clips
 
 // Return codes
 #define RET_OK          (0)                         // Normal end
@@ -37,13 +90,17 @@
  * Global variables
  * 
  ***/
-FILE *controllerTTY;                                // The stream over which we talk to the exhibit controller
+FILE *ctlIn;                                        // The input stream for the exhibit controller
+FILE *ctlOut;                                       // The output stream for the controller
 bool running = true;                                // When this goes false (e.g., the stop command), we shut down
 
-// Inter-thread communication between a thread that wants to change the clip that's playing and main loop.
-// To change the clip, do a piLock(0), then set newClip to the desired clip. Then set switchClip to true 
-// and do a piUnlock. The main loop checks switchClip to determine if someone has set it. If so it does a 
-// piLock(0), copies newClip, sets switchClip ot false and does a piUnlock(0).
+// Inter-thread communication between a thread that wants to change the clip that's playing and 
+// main loop. To change the clip, do a piLock(LOCK_CLIP), then set newClip to the desired clip. 
+// Then set switchClip to true and do a piUnlock(LOCK_CLIP). The main loop checks switchClip to 
+// determine if someone has set it. If so it does a piLock(LOCK_CLIP), copies newClip, sets 
+// switchClip to false and  does a piUnlock(LOCK_CLIP). Note that with this protocol, it's 
+// possible to overwrite somebody else's clip change before it's processed. If that's not what 
+// what you want, check switchClip to see that it's false before you change newClip.
 clip_t newClip;
 bool switchClip = false;
 
@@ -69,7 +126,8 @@ void onHelp(int n, char word[MAX_WORDS][MAX_WSIZE]) {
  * 
  * Command handler for play command
  * 
- * play c   Plays clip c; 0 <= c < CLIP_COUNT
+ * play c   Play clip c; 0 <= c < CLIP_COUNT
+ * !play c  Same as play, but issued by controller
  * 
  ***/
 void onPlay(int n, char word[MAX_WORDS][MAX_WSIZE]) {
@@ -82,10 +140,10 @@ void onPlay(int n, char word[MAX_WORDS][MAX_WSIZE]) {
         printf("Clip number must be 0 to %d\n", CLIP_COUNT);
         return;
     }
-    piLock(0);                  // Get the lock
+    piLock(LOCK_CLIP);      // Get the lock
     newClip = clips[c];
     switchClip = true;
-    piUnlock(0);
+    piUnlock(LOCK_CLIP);    // Release the lock
 }
 
 /***
@@ -93,6 +151,7 @@ void onPlay(int n, char word[MAX_WORDS][MAX_WSIZE]) {
  * Command handler for stop command
  * 
  * stop     Stops media player and exits program
+ * !stop    Same as stop, but issued from controller
  * 
  ****/
 void onStop(int n, char word[MAX_WORDS][MAX_WSIZE]) {
@@ -100,6 +159,17 @@ void onStop(int n, char word[MAX_WORDS][MAX_WSIZE]) {
     running = false;
 }
 
+/***
+ * 
+ * Command handler for !version command
+ * 
+ * !version     Send MediaPlayer command set version information to controller
+ *              Only issued by controller
+ * 
+ ***/
+void onVersion(int n, char word[MAX_WORDS][MAX_WSIZE]) {
+    fprintf(ctlOut, "!mediaplayer %d\n", CMD_SET_VERS);         // Tell controller what command set we speak
+}
 
 // Command registry data structure
 typedef struct cmd_t {
@@ -107,7 +177,8 @@ typedef struct cmd_t {
     void (*handler)(int n, char word[MAX_WORDS][MAX_WSIZE]);    // The command handler for this command
 } cmd_t;
 
-// The registry of keyboard commands. The last command must be {"__END__", NULL}.
+// The registry of keyboard-issued commands aimed at MediaPlayer. 
+// The last command must be {"__END__", NULL}.
 cmd_t kbRegistry[] = {
     {"help", onHelp},
     {"h",    onHelp},
@@ -116,10 +187,12 @@ cmd_t kbRegistry[] = {
     {"__END__", NULL}
 };
 
-// The registry of controller commands. The last command must be {"__END__", NULL}.
+// The registry of controller-issued commands aimed at MediaPlayer. 
+// The last command must be {"__END__", NULL}.
 cmd_t controllerRegistry[] = {
     {"!play", onPlay},
     {"!stop", onStop},
+    {"!version", onVersion},
     {"__END__", NULL}
 };
 
@@ -157,10 +230,7 @@ PI_THREAD(keyboardThread) {
         if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
             // Send commands beginning with '!' to controller; others are local
             if (buffer[0] == '!') {
-                int rc = fprintf(controllerTTY, &buffer[1]); 
-                if (rc < 0) {
-                    printf("Couldn't send command to controller. rc = %d\n", rc);
-                }
+                fprintf(ctlOut, &buffer[1]);
             } else {
                 doCommand(buffer, kbRegistry);
             }
@@ -171,17 +241,17 @@ PI_THREAD(keyboardThread) {
 
 /***
  * 
- * controllerThread -- talk to the exhibit controller. Echo whatever it says to stdout, prefaced 
- * by "[controller] ". Watch for lines beginning with "!" which indicates a command for us, to be 
- * executed using the same sort of mechanism (and the same handler signatures) and the keyboard 
- * commands.
+ * controllerThread -- get input from the exhibit controller. Echo whatever it says to stdout, 
+ * prefaced by "[controller] ". Watch for lines beginning with "!" which indicates a command 
+ * directed at MediaPlayer, to be executed using the same sort of mechanism (and the same handler 
+ * signatures) and the keyboard commands.
  * 
  ***/
 PI_THREAD(controllerThread) {
     char buffer[MAX_LINE_LENGTH];
 
     while (1==1) {
-        if (fgets(buffer, sizeof(buffer), controllerTTY) != NULL) {
+        if (fgets(buffer, sizeof(buffer), ctlIn) != NULL) {
             printf("[controller] %s", buffer);
             if (buffer[0] == '!') {
                 doCommand(buffer, controllerRegistry);
@@ -205,23 +275,46 @@ int main(int argc, char* argv[]) {
     puts(BANNER);
     puts("Type \"help\" for list of commands");
 
-    // Get the keyboard input thread going
+    // Get the keyboard input thread going. All stdin activity is done on keyboardThread
+    // stdout and ctlOut activity can be done by any thread.
     if (piThreadCreate(keyboardThread) != 0) {
         puts("Failed to create keyboard thread.");
         return RET_KTCF;
     }
 
-    // Get the connection to the exhibit controller going
-    controllerTTY = fopen(CONTROLLER_TTY, "w+");
-    if (controllerTTY == NULL) {
-        printf("Failed to open controller TTY. Error: %s\n", strerror(errno));
+    // Get the connections to the exhibit controller (ctlIn and ctlOut) going
+    // ctlIn needs line buffering mode and no echoing of characters
+    ctlIn = fopen(CONTROLLER_TTY, "r");
+    if (ctlIn == NULL) {
+        printf("Failed to open ctlIn. Error: %s\n", strerror(errno));
         return RET_OCTF;
     }
+    if (setvbuf(ctlIn, NULL, _IOLBF, MAX_LINE_LENGTH) != 0) {
+        printf("Failed to set ctlIn buffer mode. Error: %s\n", strerror(errno));
+        return RET_OCTF;
+    }
+    struct termios t;
+    if (tcgetattr(ctlIn->_fileno, &t) != 0) {
+        printf("Failed to get termios for ctlIn. Error: %d, (%s)\n", errno, strerror(errno));
+        return(RET_OCTF);
+    }
+    t.c_lflag &= ~ECHO;
+    if (tcsetattr(ctlIn->_fileno, TCSANOW, &t) != 0) {
+        printf("Failed to set termios for ctlIn. Error: %d, (%s)\n", errno, strerror(errno));
+    }
+
+    // ctlOut needs append mode
+    ctlOut = fopen(CONTROLLER_TTY, "a");
+    if (ctlOut == NULL) {
+        printf("Failed to open ctlOut. Error: %s\n", strerror(errno));
+        return RET_OCTF;
+    }
+
+    // Get the controller thread going all ctlIn activity is done on controllerThread
     if (piThreadCreate(controllerThread) != 0) {
         puts("Failed to create controller thread.");
         return RET_CTCF;
     }
-    fprintf(CONTROLLER_TTY, "!mediaplayer %d\n", CMD_SET_VERS);   // Tell controller what command set we speak
 
     // Set things up to play the exhibit's media
     inst = libvlc_new (0, NULL);                    // Load and initialize the libVLC engine
@@ -259,13 +352,15 @@ int main(int argc, char* argv[]) {
     // Main loop. Do until running goes false
     while (running) {
         if (switchClip) {
-            piLock(0);
+            piLock(LOCK_CLIP);
             curClip = newClip;
             switchClip = false;
-            piUnlock(0);
+            piUnlock(LOCK_CLIP);
             libvlc_media_player_set_time(mp, curClip.start);
         }
-        if (libvlc_media_player_get_time(mp) >= curClip.end) {
+        libvlc_time_t curTime = libvlc_media_player_get_time(mp) + MEDIA_DELTA_MS;
+        if (curTime >= curClip.end) {
+            printf("Current time: %jd, Clip end: %jd\n", curTime, curClip.end);
             if (curClip.clipType != loop) {
                 curClip = clips[idle];
             }
--- mediadef.h
+++ mediadef.h
@@ -1,24 +1,58 @@
 /***
  * 
- * The media definition for MediaPlayer
+ * The media definition file for MediaPlayer MediaPlayer 
+ * Version 0.10, February 2022
  * 
- ****/
-
-#define MEDIA_PATH      "/home/pi/Downloads/Sequence02Low.mp4" // The video containing all the clips for the exhibit.
+ * This file is a part of the media clip player for the PTMSC Pinto Abalone 
+ * exhibit. See the file MediaPlayer.c for general information.
+ * 
+ * This file describes the video file used by the exhibit to play clips as 
+ * needed as the visior interacts with the exhibit. All of the clips are 
+ * contained in a single video file. A clip is described by the times at which 
+ * it begins and ends, in ms relative to the beginning of the video, together 
+ * with information describing how it should be handled -- looped, played 
+ * through only once and so on. A clip description is of clip_t. The complete 
+ * collection of clips in the video file is in the array named clips.
+ * 
+ ***
+ * 
+ * Copyright (C) 2020-2022 D.L. Ehnebuske
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. 
+ * 
+***/
+#define MEDIA_PATH      "/home/pi/Downloads/Sequence03Low.mp4" // The video containing all the clips for the exhibit.
 
 enum clipNames {
-    idle,
-    decide1,
-    place1,
-    place2,
-    miss,
-    decide2,
-    CLIP_COUNT
+    idle,               // This is the idle loop clip -- the clip that plays when nothing else is
+    decide1,            // This clip shows a diver inspecting an outplacement site to see if it's a good one
+    place1,             // This clip shows a diver doing an outplacement
+    place2,             // This clip shows a diver doing an outplacement
+    miss,               // This clip shows a diver doing an outplacement incorrectly
+    decide2,            // This clip shows a diver inspecting a potntial site to see if it's a good one
+    CLIP_COUNT          // This is not a clip name. Its value is the number of clips. It must be last.
 };
 
 enum clipTypes {
-    playOnce,
-    loop
+    playOnce,           // Play the clip once and then revert to idle. It's okay to interrupt it with an new clip
+    playThrough,        // Play the clip once and the revert to idle. No new clip until it finishes
+    loop                // Play the clip over and over. It's okay to interrupt it with a new clip
 };
 
 typedef struct clip_t {
@@ -28,9 +62,9 @@ typedef struct clip_t {
     libvlc_time_t end;              // Time clip ends (ms)
 } clip_t;
 
-// The individual clip definitions
+// The collection clip definitions
 clip_t clips[] = {
-    {idle, loop, 567, 10100}, 
+    {idle, loop, 0, 13300}, 
     {decide1, playOnce, 13300, 34667},
     {place1, playOnce, 34667, 71533},
     {place2, playOnce, 71533, 93267},
